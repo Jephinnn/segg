c30a3faf62d93b6a0d6d098b548cd424de2f48d9
Jephinnn <154929967+Jephinnn@users.noreply.github.com>
2025-02-19 09:21:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,10 +58,9 @@ function exibirNotificacoes(notificacoes) {
     notificacoes.forEach(notificacao => {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${notificacao.notificacao_id}</td>
             <td>${notificacao.colaborador}</td>
             <td>${notificacao.mensagem}</td>
-			<td>${notificacao.mensagem2}</td>
+	    <td>${notificacao.mensagem2}</td>
             <td>${notificacao.observacoes}</td>
             <td>${new Date(notificacao.dataHora).toLocaleString()}</td>
         `;
